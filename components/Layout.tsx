4f76ad098a180964d2578e07a2db0d723aa38c28
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-10-16 17:19:54 -0400

    fix(layout): adjust mobile padding

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex justify-center">
-      <div className="bg-white w-full border-x border-slate-200 min-h-screen md:mx-8 lg:mx-16 mx-0 p-10">
+      <div className="bg-white w-full border-x border-slate-200 min-h-screen md:mx-8 lg:mx-16 mx-0 px-4 py-10 md:px-10 md:py-20">
         {children}
       </div>
     </div>
