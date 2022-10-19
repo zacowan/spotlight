eb70edf7ac8b0612593e3bd1c179c0fa6baef501
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-10-19 14:38:09 -0400

    feat(favicon): add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import React from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#a5b4fc"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
