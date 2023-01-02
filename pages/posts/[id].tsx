aa3c7833827de4e9450ee9ee9f2312253b681330
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-01-02 10:25:28 -0500

    fix(post): add social card image

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -48,11 +48,14 @@ const Post: React.FC<Props> = ({ post }) => {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+        <meta
+          property="og:image"
+          content="https://www.zacowan.com/resources/social_card.png"
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:image"
-          content={post.featuredImage.node.sourceUrl}
+          content="https://www.zacowan.com/resources/social_card.png"
         />
       </Head>
       <Navigation />
